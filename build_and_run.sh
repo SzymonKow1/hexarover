9689544fec58e7bd612c81b229a0afa5be06f02e
Szymon Kowalski <szymon.kowalski580@gmail.com>
2026-05-20 00:29:58 +0200

    adding clean build to sh & motivational message

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,6 +4,11 @@ set -e
 
 cd ~/ros2_ws
 
+# Sprawdzenie czy podano argument czyszczący
+if [[ "$1" == "clean" || "$1" == "--clean" || "$1" == "-c" ]]; then
+    echo "0/3 Czyszczenie obszaru roboczego (usunięcie build, install, log)..."
+    rm -rf build/ install/ log/
+fi
 
 echo "1/3 Budowanie projektu"
 colcon build --symlink-install
@@ -14,4 +19,4 @@ source install/setup.bash
 echo "3/3 Odpalenie projektu"
 ros2 launch hexarover_bringup hexarover.launch.py
 
-echo "Gotowe :)"
+echo "Trzymaj sie moze i nie dziala narazie ale dasz rade"

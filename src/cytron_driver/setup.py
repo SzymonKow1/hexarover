e952af8ab466d76fa7c260157f41cd734097c769
SebastianKuczera <s198118@student.pg.edu.pl>
2026-05-19 19:56:15 +0200

    adding temp imu_node to cytron_driver

--- src/cytron_driver/setup.py
+++ src/cytron_driver/setup.py
@@ -25,6 +25,7 @@ setup(
     entry_points={
         'console_scripts': [
             'cytron_node = cytron_driver.cytron_node:main',
+            'imu_node = cytron_driver.imu_node:main',
         ],
     },
 )

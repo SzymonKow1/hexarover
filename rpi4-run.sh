16df626522da2d075ba32cc5bb1c0a63012abc83
Szymon Kowalski <szymon.kowalski580@gmail.com>
2026-06-10 05:14:17 +0200

    code clean-up

---
+++ rpi4-run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+cd ~/ros2_ws
+
+# Sprawdzenie czy podano argument czyszczący
+if [[ "$1" == "clean" || "$1" == "--clean" || "$1" == "-c" ]]; then
+    echo "0/3 Czyszczenie obszaru roboczego (usunięcie build, install, log)..."
+    rm -rf build/ install/ log/
+fi
+
+echo "1/3 Budowanie projektu"
+colcon build --symlink-install
+
+echo "2/3 Dodanie do sciezki"
+source install/setup.bash
+
+echo "3/3 Odpalenie projektu"
+ros2 launch hexarover_bringup rpi4.launch.py
+
+echo "Trzymaj sie moze i nie dziala narazie ale dasz rade"
